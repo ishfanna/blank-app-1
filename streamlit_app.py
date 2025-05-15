497631ccaebea96865f861aa52021c791ac0d220
ishfanna <beyawoozdie@gmail.com>
2025-05-15 19:53:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
-st.title("halloo ini udinara")
+st.title("hello this is udinara")
 st.write(
-    "dibawah ini foto aku sama asni"
+    "Below is a photo of me and Asni"
 )
 st.image("views/20250515_193947.jpg", width=200)
 st.write("\n")
 st.subheader("dindondun")
-st.write("dinara love asni") 
+st.write(
+    "dinara love asni"
+) 
+ 
